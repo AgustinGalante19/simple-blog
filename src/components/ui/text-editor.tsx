7c8700a2b891e5e25deb9c71861168844c58f843
Agustin Galante <agustin.galante.19@outlook.es>
2024-01-05 21:07:09 -0300

    Underline and Code on rich-text-editor

--- src/components/ui/text-editor.tsx
+++ src/components/ui/text-editor.tsx
@@ -2,15 +2,18 @@
 
 import { useEditor, EditorContent, type Editor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import { Bold, Strikethrough, Italic, Heading2 } from "lucide-react";
+import { Bold, Strikethrough, Italic, Heading2, Code } from "lucide-react";
 import { Toggle } from "./toggle";
 import Heading from "@tiptap/extension-heading";
+import CodeExt from "@tiptap/extension-code";
+import Underline from "@tiptap/extension-underline";
+import { UnderlineOutlined } from "@ant-design/icons";
 
 function Toolbar({ editor }: { editor: Editor | null }) {
   if (!editor) return null;
 
   return (
-    <div className='space-x-1 border bg-slate-100/30 p-1 border-gray-400/60 rounded-t-md '>
+    <div className='space-x-1 border bg-slate-100/30 p-1 border-gray-400/60 rounded-t-md flex'>
       <Toggle
         size='sm'
         variant='outline'
@@ -40,10 +43,26 @@ function Toolbar({ editor }: { editor: Editor | null }) {
       <Toggle
         size='sm'
         variant='outline'
+        pressed={editor.isActive("underline")}
+        onPressedChange={() => editor.chain().focus().toggleUnderline().run()}
+      >
+        <UnderlineOutlined className='h-4 w-4' />
+      </Toggle>
+      <Toggle
+        size='sm'
+        variant='outline'
         pressed={editor.isActive("strike")}
         onPressedChange={() => editor.chain().focus().toggleStrike().run()}
       >
         <Strikethrough className='h-4 w-4' />
+      </Toggle>
+      <Toggle
+        size='sm'
+        variant='outline'
+        pressed={editor.isActive("code")}
+        onPressedChange={() => editor.chain().focus().toggleCode().run()}
+      >
+        <Code className='h-4 w-4' />
       </Toggle>
     </div>
   );
@@ -65,6 +84,12 @@ function TextEditor({
           levels: [2],
         },
       }),
+      CodeExt.configure({
+        HTMLAttributes: {
+          class: "p-1 bg-slate-100",
+        },
+      }),
+      Underline,
     ],
     content: description,
     editorProps: {
