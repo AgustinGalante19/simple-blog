027686e1045c8d59fa8bd2fa4929532a171eb27b
Agustin Galante <agustin.galante.19@outlook.es>
2024-01-11 08:25:31 -0300

    handeling erros on post detail

--- src/app/api/post/[id]/route.ts
+++ src/app/api/post/[id]/route.ts
@@ -1,36 +1,54 @@
-import client from "@/lib/prisma";
-import { Post } from "@prisma/client";
-import { NextResponse } from "next/server";
+import client from "@/lib/prisma"
+import { Post } from "@prisma/client"
+import { NextResponse } from "next/server"
 
 export async function GET(
   _: Request,
   {
     params,
   }: {
-    params: { id: string };
+    params: { id: string }
   }
 ): Promise<NextResponse<ApiResponse<Post>>> {
-  const post = await client.post.findUnique({
-    where: {
-      id: params.id,
-    },
-    include: {
-      User: {
-        select: {
-          username: true,
-          name: true,
-          lastname: true,
+  try {
+    const post = await client.post.findUnique({
+      where: {
+        id: params.id,
+      },
+      include: {
+        User: {
+          select: {
+            username: true,
+            name: true,
+            lastname: true,
+          },
         },
       },
-    },
-  });
-  if (post) {
-    return NextResponse.json({ data: [post], error: [], result: "ok" });
+    })
+    if (!post) {
+      return NextResponse.json(
+        {
+          data: [],
+          error: ["Post not found"],
+          result: "error",
+        },
+        {
+          status: 404,
+        }
+      )
+    }
+    return NextResponse.json({ data: [post], error: [], result: "ok" })
+  } catch (err) {
+    console.log(err)
+    return NextResponse.json(
+      {
+        data: [],
+        error: ["Unexpected error"],
+        result: "error",
+      },
+      {
+        status: 500,
+      }
+    )
   }
-
-  return NextResponse.json({
-    data: [],
-    error: ["Post not found"],
-    result: "error",
-  });
 }
--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,78 +1,108 @@
-"use client";
+"use client"
 
-import useCases, { PostIdResponse } from "@/api/useCases";
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import DOMPurify from "isomorphic-dompurify";
-import Image from "next/image";
-import PostDeailtsLoader from "@/components/ui/post-detail-loader";
+import useCases, { PostIdResponse } from "@/api/useCases"
+import { useParams } from "next/navigation"
+import { useEffect, useState } from "react"
+import DOMPurify from "isomorphic-dompurify"
+import Image from "next/image"
+import PostDeailtsLoader from "@/components/ui/post-detail-loader"
+import { AxiosError } from "axios"
 
 function Post() {
-  const { id } = useParams();
-  const [isLoading, setIsLoading] = useState(true);
-  const [post, setPost] = useState<PostIdResponse | null>(null);
+  const { id } = useParams()
+  const [isLoading, setIsLoading] = useState(true)
+  const [post, setPost] = useState<PostIdResponse | null>(null)
+  const [error, setError] = useState<{
+    isFailed: boolean
+    message: string | undefined
+  }>({
+    isFailed: false,
+    message: "",
+  })
 
   useEffect(() => {
     if (typeof id === "string") {
-      setIsLoading(true);
+      setIsLoading(true)
       useCases.posts
         .getOne(id)
         .then((response) => {
-          const postResponse = response.data.data[0];
+          const postResponse = response.data.data[0]
           setPost({
             ...postResponse,
             createdAt: new Date(postResponse.createdAt),
-          });
+          })
         })
-        .catch((err) => console.log("algo salio mal: ", err))
-        .finally(() => setIsLoading(false));
+        .catch((err: AxiosError<ApiResponse<string>>) => {
+          console.log(err)
+          setError({
+            isFailed: true,
+            message: err.response?.data.error[0],
+          })
+        })
+        .finally(() => setIsLoading(false))
     }
-  }, []);
+  }, [])
 
   const sanitizedContent = DOMPurify.sanitize(
     post?.content ?? "<p>no content :(</p>",
     {
       USE_PROFILES: { html: true },
     }
-  );
+  )
 
-  return !isLoading && post ? (
-    <article className='max-md:mx-0 max-md:w-full w-[700px] bg-white p-16'>
-      <div className='max-w-3xl mx-auto'>
-        {post.headerImage && post.headerImage !== "" && (
-          <div className='relative'>
-            <Image
-              src={post.headerImage}
-              alt='header image'
-              className='object-cover w-[720px] h-[250px] mx-auto'
-              height={250}
-              width={720}
-            />
-          </div>
-        )}
-        <div className='py-2 flex items-center justify-between mb-4'>
-          <div>
-            <span className='font-bold text-lg'>
-              {post.User.name} {post.User.lastname}
-            </span>
-            <span className='block text-sm text-gray-400'>
-              @{post.User.username}
-            </span>
-          </div>
-          <div className='text-xs text-gray-400'>
-            <span>{post.createdAt.toDateString()}</span>
-          </div>
+  if (error.isFailed) {
+    return (
+      <div className='max-md:mx-0 max-md:w-full w-[700px] flex flex-col min-h-screen bg-white'>
+        <div className='p-6 ronded-md mt-8'>
+          <span className='text-3xl font-bold  flex justify-center text-gray-400'>
+            {error.message}
+          </span>
         </div>
-        <section
-          dangerouslySetInnerHTML={{
-            __html: sanitizedContent,
-          }}
-        />
       </div>
-    </article>
-  ) : (
+    )
+  }
+
+  return isLoading ? (
     <PostDeailtsLoader />
-  );
+  ) : !isLoading && !post ? (
+    <p>not found</p>
+  ) : (
+    post && (
+      <article className='max-md:mx-0 max-md:w-full w-[700px] bg-white p-16'>
+        <div className='max-w-3xl mx-auto'>
+          {post.headerImage && post.headerImage !== "" && (
+            <div className='relative'>
+              <Image
+                src={post.headerImage}
+                alt='header image'
+                className='object-cover w-[720px] h-[250px] mx-auto'
+                height={250}
+                width={720}
+              />
+            </div>
+          )}
+          <div className='py-2 flex items-center justify-between mb-4'>
+            <div>
+              <span className='font-bold text-lg'>
+                {post.User.name} {post.User.lastname}
+              </span>
+              <span className='block text-sm text-gray-400'>
+                @{post.User.username}
+              </span>
+            </div>
+            <div className='text-xs text-gray-400'>
+              <span>{post.createdAt.toDateString()}</span>
+            </div>
+          </div>
+          <section
+            dangerouslySetInnerHTML={{
+              __html: sanitizedContent,
+            }}
+          />
+        </div>
+      </article>
+    )
+  )
 }
 
-export default Post;
+export default Post
